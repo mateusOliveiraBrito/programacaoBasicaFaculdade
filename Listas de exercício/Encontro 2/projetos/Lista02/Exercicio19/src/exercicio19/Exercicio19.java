a3aa77b0669e5a7a8075756f5e2f16251d93e546
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-04 19:50:32 -0300

    :tada: Implementa projeto exercicio 19 lista 02;

---
+++ Listas de exercício/Encontro 2/projetos/Lista02/Exercicio19/src/exercicio19/Exercicio19.java
@@ -0,0 +1,50 @@
+package exercicio19;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercicio19 {
+
+    public static void main(String[] args) {
+        int numero, codigo;
+        double saldo, valor;
+        String operacao;
+
+        DecimalFormat d = new DecimalFormat("#.##");
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Informe o número da conta: ");
+        numero = s.nextInt();
+        System.out.print("Informe o saldo da conta: ");
+        saldo = s.nextDouble();
+        System.out.println("");
+
+        System.out.println("Número da conta: " + numero);
+        System.out.println("Saldo da conta: " + d.format(saldo));
+        System.out.println("");
+
+        System.out.println("Informe o código da operação que você deseja: \n1-Depósito \n2-Saque");
+        System.out.print("Opção: ");
+        codigo = s.nextInt();
+        System.out.println("");
+
+        if (codigo == 1) {
+            operacao = "Depósito";
+            System.out.print("Informe um valor de " + operacao + ": ");
+            valor = s.nextDouble();
+            saldo += valor;
+        } else if (codigo == 2) {
+            operacao = "Saque";
+            System.out.print("Informe um valor de " + operacao + ": ");
+            valor = s.nextDouble();
+            saldo -= valor;
+        }
+        
+        System.out.println("Novo saldo da conta: ");
+        System.out.println("Saldo da conta: " + d.format(saldo));
+        
+        if (saldo < 0) {
+            System.out.println("Conta estourada!");
+        }
+    }
+}
