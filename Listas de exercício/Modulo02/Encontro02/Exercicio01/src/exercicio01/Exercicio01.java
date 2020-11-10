e454a3580a7b2b874142b78ab6eb7f6960a5eed2
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-09 21:15:08 -0300

    :tada: Organiza diretório de pastas e implementa projeto de exercicio01 do encontro 2, modulo 2;

---
+++ Listas de exercício/Modulo02/Encontro02/Exercicio01/src/exercicio01/Exercicio01.java
@@ -0,0 +1,14 @@
+package exercicio01;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        int inicio = 250;
+        int limite = 500;
+        
+        while (inicio <= limite) {
+            System.out.println(inicio + " ");
+            inicio++;
+        }
+    }
+}
