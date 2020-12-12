6439dd354d8fa5b72a29c9839fe15618b75a7ec7
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-12-11 23:26:39 -0300

    :tada: Implementa exercício 5.27 - módulo 3;

---
+++ Listas de exercício/Modulo03/Encontro06/Exercicio527/src/exercicio527/Exercicio527.java
@@ -0,0 +1,48 @@
+package exercicio527;
+
+import java.util.Scanner;
+
+public class Exercicio527 {
+
+    public static void main(String[] args) {
+        final int LINHAS = 5;
+        final int COLUNAS = 3;
+
+        Tabela tabela = new Tabela(LINHAS, COLUNAS);
+        Scanner s = new Scanner(System.in);
+
+        //preenche tabela
+        for (int i = 0; i < LINHAS; i++) {
+            System.out.print("\nINFORME UM VALOR PARA AS COLUNAS DA LINHA " + (i + 1) + " DA TABELA: \n");
+            for (int j = 0; j < COLUNAS; j++) {
+                System.out.print("Informe um valor para a coluna " + (j + 1) + " desta linha: ");
+                int n = s.nextInt();
+                tabela.tabela[i][j] = n;
+                tabela.tabelaInicial[i][j] = n;
+            }
+        }
+
+        System.out.println("\nTabela sem alterações: ");
+        tabela.imprimeTabela();
+
+        System.out.println("\nTabela com menores e maiores números das linhas trocados: ");
+        tabela.trocaMaiorComMenorPorLinha();
+        tabela.imprimeTabela();
+
+        System.out.println("\nTabela com colunas trocadas: ");
+        tabela.trocaColunas();
+        tabela.imprimeTabela();
+
+        System.out.println("\nTabela com linhas trocadas: ");
+        tabela.trocaLinhas();
+        tabela.imprimeTabela();
+
+        System.out.println("\nTabela com colunas trocadas pela soma mínima: ");
+        tabela.substituiColunasPelaSomaMinima();
+        tabela.imprimeTabela();
+        
+        System.out.println("\nTabela inicial com colunas ordenadas seguindo o critério: ");
+        tabela.ordenaTabela();
+        tabela.imprimeTabela();
+    }
+}
---
+++ Listas de exercício/Modulo03/Encontro06/Exercicio527/src/exercicio527/Tabela.java
@@ -0,0 +1,244 @@
+package exercicio527;
+
+import java.util.Arrays;
+
+public class Tabela {
+
+    private int linhas;
+    private int colunas;
+    public int[][] tabela;
+    private int[][] tabelaAuxiliar;
+    public int[][] tabelaInicial;
+    private int[] indicesLinhas;
+
+    public Tabela(int linhas, int colunas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.tabela = new int[this.linhas][this.colunas];
+        this.tabelaAuxiliar = new int[this.linhas][this.colunas];
+        this.tabelaInicial = new int[this.linhas][this.colunas];
+        this.indicesLinhas = new int[this.linhas];
+    }
+
+    public void imprimeTabela() {
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                System.out.print(tabela[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void zeraTabelaAuxiliar() {
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                tabelaAuxiliar[i][j] = 0;
+            }
+        }
+    }
+
+    public void trocaMaiorComMenorPorLinha() {
+        int maior = 0;
+        int menor = 0;
+        int linhaMenor = 0, colunaMenor = 0;
+        int linhaMaior = 0, colunaMaior = 0, aux = 0;
+        boolean primeiroLoop = true;
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                if (primeiroLoop) {
+                    primeiroLoop = false;
+                    menor = tabela[i][j];
+                    maior = tabela[i][j];
+                    linhaMaior = i;
+                    colunaMaior = j;
+                    linhaMenor = i;
+                    colunaMenor = j;
+                }
+
+                if (tabela[i][j] > maior) {
+                    maior = tabela[i][j];
+                    linhaMaior = i;
+                    colunaMaior = j;
+                }
+
+                if (tabela[i][j] < menor) {
+                    menor = tabela[i][j];
+                    linhaMenor = i;
+                    colunaMenor = j;
+                }
+
+                if (j == this.colunas - 1) {
+                    tabela[linhaMenor][colunaMenor] = maior;
+                    tabela[linhaMaior][colunaMaior] = menor;
+                }
+            }
+            primeiroLoop = true;
+            menor = 0;
+            maior = 0;
+        }
+    }
+
+    public void trocaColunas() {
+        boolean primeiroLoop = true;
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+
+                if (primeiroLoop) {
+                    primeiroLoop = false;
+                    tabelaAuxiliar[i][j] = tabela[i][colunas - 1];
+                }
+
+                if (j <= colunas - 2) {
+                    tabelaAuxiliar[i][j + 1] = tabela[i][j];
+                }
+            }
+            primeiroLoop = true;
+        }
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                tabela[i][j] = tabelaAuxiliar[i][j];
+            }
+        }
+    }
+
+    public void trocaLinhas() {
+        zeraTabelaAuxiliar();
+        boolean primeiroLoop = true;
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+
+                if (primeiroLoop) {
+                    primeiroLoop = false;
+                    if (i <= linhas - 2) {
+                        tabelaAuxiliar[i][j] = tabela[i + 1][j];
+                    } else {
+                        tabelaAuxiliar[i][j] = tabela[i][j];
+                    }
+                }
+
+                if (i <= linhas - 2) {
+                    tabelaAuxiliar[i][j] = tabela[i + 1][j];
+                } else {
+                    tabelaAuxiliar[i][j] = tabela[i][j];
+                }
+
+                if (i == linhas - 1) {
+                    tabelaAuxiliar[linhas - 1][j] = tabela[0][j];
+                }
+            }
+            primeiroLoop = true;
+        }
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                tabela[i][j] = tabelaAuxiliar[i][j];
+            }
+        }
+        zeraTabelaAuxiliar();
+    }
+
+    public void substituiColunasPelaSomaMinima() {
+        int soma = 0;
+        int menorSoma = 0;
+        int indiceMenor = 0;
+        boolean primeiroLoop = true;
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                if (primeiroLoop) {
+                    for (int k = 0; k < this.colunas; k++) {
+                        soma += tabela[i][k];
+                        menorSoma = soma;
+                        primeiroLoop = false;
+                    }
+                } else {
+                    soma = 0;
+                    soma += tabela[i][j];
+                }
+            }
+
+            if (soma < menorSoma) {
+                menorSoma = soma;
+                indiceMenor = i;
+            }
+        }
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                tabelaAuxiliar[i][j] = tabela[indiceMenor][j];
+            }
+        }
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                tabela[i][j] = tabelaAuxiliar[i][j];
+            }
+        }
+    }
+
+    public void ordenaTabela() {
+        int[] valoresDosIndices = retornaValoresMaioresIndicesOrdenados(tabelaInicial);
+        int[][] auxiliar = new int[this.linhas][this.colunas];
+        int controle = 0;
+        int contLoops = 0;
+
+        for (int i = 0; i < this.linhas; i++) {
+            if (tabelaInicial[i][0] == valoresDosIndices[controle]) {
+                for (int j = 0; j < this.colunas; j++) {
+                    auxiliar[controle][j] = tabelaInicial[i][j];
+                }
+                if (i <= this.linhas) {
+                    i = 0;
+                }
+                controle++;
+            } else {
+                continue;
+            }
+            if (i <= this.linhas) {
+                    i = 0;
+                }
+            contLoops++;
+        }
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                tabela[i][j] = auxiliar[i][j];
+            }
+        }
+    }
+
+    public int[] retornaValoresMaioresIndicesOrdenados(int[][] matriz) {
+        int maior = 0;
+        String indicesCompactados = "";
+        for (int i = 0; i < this.linhas; i++) {
+            indicesCompactados += matriz[i][0] + ",";
+        }
+
+        String[] indices = indicesCompactados.split(",");
+
+        int[] indicesOrdenadosCrescente = new int[indices.length];
+        int[] indicesOrdenadosDecrescentes = new int[indices.length];
+
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                this.indicesLinhas[i] = i;
+            }
+        }
+
+        for (int i = 0; i < indices.length; i++) {
+            indicesOrdenadosCrescente[i] = Integer.parseInt(indices[i]);
+        }
+
+        Arrays.sort(indicesOrdenadosCrescente);
+
+        int cont = indices.length;
+        for (int numero : indicesOrdenadosCrescente) {
+            indicesOrdenadosDecrescentes[cont - 1] = numero;
+            cont--;
+        }
+
+        return indicesOrdenadosDecrescentes;
+    }
+}
